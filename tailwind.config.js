e434e00e1d8143e1db9f4ac7c0831d9f9af24d51
William Wang <williamblue543@gmail.com>
2025-03-28 19:20:19 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    // include any other paths that contain your HTML/JS files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // If you want to use any Tailwind plugins, list them here.
+    // For example:
+    // require('@tailwindcss/forms'),
+  ],
+};
